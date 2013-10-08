a1e0061b7541ffa1ad406091bb2100f2d685842b
xy-showen <568828193@qq.com>
2013-10-08 16:50:30 +0800

    1008

--- clientTest.js
+++ clientTest.js
@@ -1,40 +1,24 @@
 var WebSocket = require("ws");
-
-
-
-var ClientWebSocket = new WebSocket("ws://192.168.2.107:6781"
-
+var ClientWebSocket = new WebSocket("ws://192.168.2.113:6781"
     , {
-
-        host: "192.168.2.107"
-
-        ,
+        host: "192.168.2.107",
         origin: "192.168.2.110"
-
         // ,pfx:"123"
-
         // ,key:"123456"
-
         // ,passphrase:"45646"
-
         // ,cert:"7465465"
-
         // ,ca:[]
-
         // ,ciphers:"4574987"
-
         // ,rejectUnauthorized:false
-
     });
 
-
-
 ClientWebSocket.on("open", function() {
+
     console.log("client has opend succ");
 
-    ClientWebSocket.send("command|@|init|12345611|channelA");
+    ClientWebSocket.send("command|@|init|123456112|channelA");
 
-    ClientWebSocket.send("command|@|enter|12345611|channelA");
+    ClientWebSocket.send("command|@|enter|123456112|channelA");
 
     //"command|@|enter|123456|channelA"
 
@@ -45,25 +29,20 @@ ClientWebSocket.on("open", function() {
         if (err) {
 
             console.log("has err when send Identity information..");
-
         } else {
-
             console.log("send Identity information succ...");
-
         }
-
     });
-
+    // setTimeout(function(){
+    //     ClientWebSocket.send("command|@|quit|12345611|channelA"); 
+    // },1000);
+    
 });
 
 
 
 ClientWebSocket.on("message", function(data, flags) {
 
-
-
     console.log("received:" + data);
-
-
 
 });
--- gameChat/configs/global.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-	WSserverPort:6789,
-	redisServerPort:6379,
-	redisServerHost:"192.168.2.107"
-};
